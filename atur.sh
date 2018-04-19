a27cd82b4ba070b2358a377424646579f1d5c9ca
Kurniawan <38490299+kurniawandata@users.noreply.github.com>
2018-04-19 09:07:51 +0700

    Update atur.sh

--- atur.sh
+++ atur.sh
@@ -15,26 +15,27 @@ echo "2. Cek keberadaan VM di vbox           ";
 echo "3. Backup semua VM di vbox             ";
 echo "4. Hapus semua backup VM               ";
 echo "5. Hapus semua VM di vbox              ";
-echo "6. Restore semua VM di vbox            ";
-echo "7. Exit                                ";
+echo "6. Restore VM backup tanpa menimpa     ";
+echo "7. Restore VM dengan menimpa           ";
+echo "8. Exit                                ";
 echo "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+";
-read -p "Masukkan Pilihan Anda antara 1 hingga 7: " choice;
+read -p "Masukkan Pilihan Anda antara 1 hingga 8: " choice;
 echo "";
 case $choice in
 1)  echo -n "Masukkan nama user: "
     read namauser
     if [ -z "$(sudo ls -A /home/$namauser/backupvm/*)" ]; then
-    echo "Belum ada backup VM"
+    echo "Terdeteksi belum ada backup VM"
     else
-    echo "Sudah ada backup VM"
+    echo "Terdeteksi sudah ada backup VM"
     fi
     ;;
 2)  echo -n "Masukkan nama user: "
     read namauser
     if [ -z "$(ls -l /home/$namauser/VirtualBox\ VMs/testing/*)" ]; then
-    echo "Tidak ada VM di linux"
+    echo "Terdeteksi tidak ada VM di linux"
     else
-    echo "Ada VM di linux"
+    echo "Terdeteksi ada VM di linux"
     fi
     ;;
 3)  echo -n "Masukkan nama user: "
@@ -42,8 +43,8 @@ case $choice in
     if [ -z "$(ls -l /home/$namauser/VirtualBox\ VMs/testing/*)" ]; then
     echo "VM ada tidak ada"
     else
-    echo "VM ada dan backup dilakukan"
-    echo "Backup 100% dilakukan"
+    echo "VM terdeteksi ada dan backup dilakukan"
+    echo "Backup 100% telah dilakukan"
     mkdir /home/$namauser/backupvm
     sudo cp -ar /home/$namauser/VirtualBox\ VMs/* /home/$namauser/backupvm
     fi
@@ -53,7 +54,7 @@ case $choice in
     if [ -z "$(ls -A /home/$namauser/backupvm/*)" ]; then
     echo "Tidak ada backup"
     else
-    echo "Backup dihapus"
+    echo "Backup telah dihapus"
     sudo rm -r /home/$namauser/backupvm/* 
     fi
     ;;   
@@ -64,10 +65,10 @@ case $choice in
     echo -n "Masukkan nama user: "
     read namauser
     if [ -z "$(ls -A /home/$namauser/backupvm/*)" ]; then
-    echo "Tidak ada backup VM, tidak bisa dihapus"
+    echo "Tidak terdeteksi ada backup VM, tidak bisa dihapus"
     else
-    echo "Backup VM ada dan VM dilakukan penghapusan"
-    echo "Penghapusan VM 100% dilakukan"
+    echo "Backup VM terdeteksi ada dan VM dilakukan penghapusan"
+    echo "Penghapusan VM 100% telah dilakukan"
     sudo rm -r /home/$namauser/VirtualBox\ VMs/*
     fi
     fi
@@ -79,15 +80,32 @@ case $choice in
     echo "Tidak dapat direstore karena tidak ada backup"
     else
 	  if [ -z "$(ls -A /home/$namauser/VirtualBox\ VMs/*)" ]; then
-    	  echo "Tidak ada VM maka restore dilakukan"
-          echo "Restore VM 100% dilakukan"
+    	  echo "Tidak ada VM terdeteksi maka restore dilakukan"
+          echo "Restore VM 100% telah dilakukan"
           sudo cp -ar /home/$namauser/backupvm/* /home/$namauser/VirtualBox\ VMs/     	     
 	  else
     	  echo "Masih ada VM tidak bisa direstore, silahkan hapus dulu VM baru direstore (Menu pilihan nomor 5)"
           fi    
     fi
     ;;   
-7)    exit
+
+7)  read -p "Apakah anda yakin akan restore dengan menimpa? y/n :" -n 1 -r
+    echo 
+    if [[ ! $REPLY =~ ^[Nn]$ ]]
+    then
+    echo -n "Masukkan nama user: "
+    read namauser
+    if [ -z "$(ls -A /home/$namauser/backupvm/*)" ]; then
+    echo "Tidak ada backup VM, tidak bisa direstore"
+    else
+    echo "Backup VM terdeteksi ada dan tetap restore dengan menimpa"
+    echo "Restore VM dengan menimpa 100% telah dilakukan"
+    sudo cp -ar /home/$namauser/backupvm/* /home/$namauser/VirtualBox\ VMs/
+    fi
+    fi
+    ;;
+
+8)    exit
     ;;
 *)    echo "Maaf, menu tidak ada"
 esac
